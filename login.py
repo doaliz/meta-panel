183a597a696fd4fc99e19206722da5adf156aafb
doaliz <142404110+doaliz@users.noreply.github.com>
2025-04-03 01:33:56 +0300

    Update login.py

--- login.py
+++ login.py
@@ -4,18 +4,19 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="Facebook Giriş", layout="centered")
 st.title("🔑 Facebook Giriş")
 
-st.markdown("Giriş yapılıyor...")
-
 components.html(
     """
     <script>
         const tokenMatch = window.location.hash.match(/access_token=([^&]+)/);
         if (tokenMatch) {
             const token = tokenMatch[1];
+            console.log("TOKEN FOUND:", token);
             localStorage.setItem("fb_token", token);
-            window.location.href = window.location.origin;  // ana sayfaya yönlendir
+            window.location.replace(window.location.origin);  // ana sayfaya yönlendir
+        } else {
+            document.write("❌ Token bulunamadı. Lütfen tekrar deneyin.");
         }
     </script>
     """,
-    height=0
+    height=100
 )
